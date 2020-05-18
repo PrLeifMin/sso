3aa6acbd801b33809f4cfa9d9e2f42c20bc2312f
PrLeifMin <847433152@qq.com>
2020-05-18 15:37:46 +0800

    sql

---
+++ sso_st.sql
@@ -0,0 +1,45 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost1
+Source Server Version : 50726
+Source Host           : localhost:3306
+Source Database       : sso
+
+Target Server Type    : MYSQL
+Target Server Version : 50726
+File Encoding         : 65001
+
+Date: 2020-05-18 15:36:48
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for sso_st
+-- ----------------------------
+DROP TABLE IF EXISTS `sso_st`;
+CREATE TABLE `sso_st` (
+  `st` varchar(255) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sso_st
+-- ----------------------------
+INSERT INTO `sso_st` VALUES ('c17d7965817d9c6f9dfa6f73cc39c840');
+INSERT INTO `sso_st` VALUES ('c17d7965817d9c6f9dfa6f73cc39c840');
+INSERT INTO `sso_st` VALUES ('c17d7965817d9c6f9dfa6f73cc39c840');
+INSERT INTO `sso_st` VALUES ('c17d7965817d9c6f9dfa6f73cc39c840');
+INSERT INTO `sso_st` VALUES ('c17d7965817d9c6f9dfa6f73cc39c840');
+INSERT INTO `sso_st` VALUES ('c17d7965817d9c6f9dfa6f73cc39c840');
+INSERT INTO `sso_st` VALUES ('8a74eca150156fdb167d3795b9e438b3');
+INSERT INTO `sso_st` VALUES ('8a74eca150156fdb167d3795b9e438b3');
+INSERT INTO `sso_st` VALUES ('8a74eca150156fdb167d3795b9e438b3');
+INSERT INTO `sso_st` VALUES ('8a74eca150156fdb167d3795b9e438b3');
+INSERT INTO `sso_st` VALUES ('c17d7965817d9c6f9dfa6f73cc39c840');
+INSERT INTO `sso_st` VALUES ('8a74eca150156fdb167d3795b9e438b3');
+INSERT INTO `sso_st` VALUES ('8a74eca150156fdb167d3795b9e438b3');
+INSERT INTO `sso_st` VALUES ('c17d7965817d9c6f9dfa6f73cc39c840');
+INSERT INTO `sso_st` VALUES ('8a74eca150156fdb167d3795b9e438b3');
+INSERT INTO `sso_st` VALUES ('c17d7965817d9c6f9dfa6f73cc39c840');
+INSERT INTO `sso_st` VALUES ('8a74eca150156fdb167d3795b9e438b3');
